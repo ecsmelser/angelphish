4880466d538bb499fced89b3ca6e56d5683a2f11
ecsmelser <160266439+ecsmelser@users.noreply.github.com>
2024-04-02 16:19:53 -0500

    Update content.js

--- AngelPhish Extension 2.0/content.js
+++ AngelPhish Extension 2.0/content.js
@@ -1,3 +1,10 @@
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.action === "logUrl") {
+        console.log("URL logged:", message.url);
+    }
+});
+
+
 chrome.webRequest.onCompleted.addListener(function(details) {
     if (details.type === "main_frame") {
         chrome.runtime.sendMessage({ action: 'logUrl', url: details.url });
