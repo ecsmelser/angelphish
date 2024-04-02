76c2d84f7cdf00f21f40d2ada2a2d5d1d5f3f82e
ecsmelser <160266439+ecsmelser@users.noreply.github.com>
2024-04-02 16:20:37 -0500

    Update background.js

--- AngelPhish Extension 2.0/background.js
+++ AngelPhish Extension 2.0/background.js
@@ -1,3 +1,10 @@
+// console.log('Extension is running!');
+
+chrome.runtime.onStartup.addListener( () => {
+    console.log(`onStartup()`);
+});
+
+
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.action === 'logUrl') {
         saveURL(message.url);
@@ -5,17 +12,18 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 });
 
 function saveURL(url) {
-    localStorage.setItem('websiteURL', 'https://example.com')
+    localStorage.setItem('websiteURL', url);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     // Retrieve URL from localStorage
-    const websiteURL = localStorage.getItem('websiteURL=');
+    const websiteURL = localStorage.getItem('websiteURL');
 
-    // Check if URL exists in localStorage
+     // Check if URL exists in localStorage
     if (websiteURL) {
         // Update HTML content with the retrieved URL
-        document.getElementById('phishing-url').innerText = 'Phishing URL: ' + websiteURL;
+    console.log('Extension is running!');
+    document.getElementById('phishing-url').innerText = 'Phishing URL: ' + websiteURL;
     } else {
         // Handle case where URL is not found in localStorage
         document.getElementById('phishing-url').innerText = 'No phishing URL found.';
